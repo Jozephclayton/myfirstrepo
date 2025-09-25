c0b58be086997315609f5055f3af40c2fbc79cf1
Joseph Clayton <jrclayton2@gmail.com>
2025-09-25 12:12:07 -0500

    added notes

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "Enter commit message: " message
-git add .
-git commit -m "$message"
-git push
-echo "done"
+git add . #
+git commit -m "$message" #ties the message to those files
+git push #this step actually only sends the files up to the server
+echo "done" #just logs a message
